141bab50347091c97fd5a873a8a6266f5c7501fa
Roger Ren <roger.xren@gmail.com>
2018-03-16 09:21:21 -0400

    [Roger] Complete flaskTest website:     1. Connected to local Spark     2. Can update Spark as request     3. Can make queries

--- flask/flaskTest/app/__init__.py
+++ flask/flaskTest/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from pyspark import SparkContext
+
+sc = SparkContext("local", "Simple App")
 app = Flask(__name__)
 app.config.from_object(Config)
+
 
 from app import routes
 
