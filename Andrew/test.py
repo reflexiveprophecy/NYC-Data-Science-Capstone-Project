7bad06cf7ef3fc21aae1e907b6221fd6574ec108
Andrew Dodd <adodd202@gmail.com>
2018-03-26 09:29:55 -0400

    Delete test.py

--- Andrew/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = 'https://gimmeproxy.com/api/getProxy'
-proxies = {
-    "http": '159.224.176.205:53281', 
-    "https": '159.224.176.205:53281'
-}
-response = requests.get(url,proxies=proxies)
-print(response.json())
